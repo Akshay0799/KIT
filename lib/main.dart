fce33657ad5f23377e8e1ded04d416d339822c1a
Akshay <30732059+Akshay0799@users.noreply.github.com>
2020-01-13 10:39:20 +0530

    First Push

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Trip Cost Calculator',
+      title: 'KIT',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -21,137 +21,24 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: HelloYou(),
+      home: Home(),
     );
   }
 }
 
-class HelloYou extends StatefulWidget {
+class Home extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _HelloYouState();
+  State<StatefulWidget> createState() => _HomeState();
 }
 
-class _HelloYouState extends State<HelloYou> {
-  String name = '';
-  TextEditingController distanceController = TextEditingController();
-  TextEditingController avgController = TextEditingController();
-  TextEditingController priceController = TextEditingController();
-
-  final _currencies = ['Dollars', 'Euro', 'Pounds', 'Rupees'];
-  String _currency = 'Dollars';
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    TextStyle textStyle = Theme.of(context).textTheme.title;
     return Scaffold(
         appBar: AppBar(
-          title: Text("APP"),
+          title: Text("KIT"),
           backgroundColor: Colors.blueAccent,
         ),
-        body: Container(
-            padding: EdgeInsets.all(15.0),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                    padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: 'e.g. 124',
-                          labelText: 'Distance',
-                          labelStyle: textStyle,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10.0))),
-                      keyboardType: TextInputType.number,
-                      controller: distanceController,
-                    )),
-                Padding(
-                  padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: 'e.g. 124',
-                        labelText: 'Distance per Unit',
-                        labelStyle: textStyle,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0))),
-                    keyboardType: TextInputType.number,
-                    controller: avgController,
-                  ),
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                        child: TextField(
-                      decoration: InputDecoration(
-                          hintText: 'e.g. 1.24',
-                          labelText: 'Price',
-                          labelStyle: textStyle,
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5.0))),
-                      keyboardType: TextInputType.number,
-                      controller: priceController,
-                    )),
-                    Container(width: 5.0 * 5),
-                    Expanded(
-                        child: DropdownButton(
-                      items: _currencies
-                          .map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      value: _currency,
-                      onChanged: (String value) {
-                        setState(() {
-                          this._currency = value;
-                        });
-                      },
-                    )),
-                  ],
-                ),
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                        child: RaisedButton(
-                      color: Theme.of(context).primaryColorDark,
-                      textColor: Theme.of(context).primaryColorLight,
-                      onPressed: () {
-                        setState(() {
-                          name = calculate();
-                        });
-                      },
-                      child: Text('Submit', style: TextStyle(fontSize: 20)),
-                    )),
-                    Container(width: 5.0 * 5),
-                    Expanded(
-                        child: RaisedButton(
-                      color: Theme.of(context).buttonColor,
-                      textColor: Theme.of(context).primaryColorDark,
-                      onPressed: () {
-                        reset();
-                      },
-                      child: Text('Reset', style: TextStyle(fontSize: 20)),
-                    )),
-                  ],
-                ),
-                Text(name)
-              ],
-            )));
-  }
-
-  String calculate() {
-    double d = double.parse(distanceController.text);
-    double fc = double.parse(avgController.text);
-    double c = double.parse(priceController.text);
-    double total = (d / c) * fc;
-    return "The Total cost of the trip is " +
-        total.toStringAsFixed(2) +
-        ' ' +
-        _currency;
-  }
-
-  void reset() {
-    distanceController.text = '';
-    avgController.text = '';
-    priceController.text = '';
+    )
   }
 }
